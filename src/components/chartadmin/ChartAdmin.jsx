049af859b94e9c2f14aa8c9c09d1527074679017
Thinh Dang <danggiathinhgocong@gmail.com>
2023-05-10 01:41:59 +0700

    fix data trash, done mailwarning

--- src/components/chartadmin/ChartAdmin.jsx
+++ src/components/chartadmin/ChartAdmin.jsx
@@ -23,12 +23,6 @@ const ChartAdmin = ({ aspect, title, data }) => {
           data={data}
           margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
         >
-          {/* <defs>
-            <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-            </linearGradient>
-          </defs> */}
           <XAxis dataKey="date" stroke="gray" />
           <YAxis />
           <CartesianGrid strokeDasharray="3 3" className="chartGrid" />
@@ -36,13 +30,6 @@ const ChartAdmin = ({ aspect, title, data }) => {
           <Legend />
           <Line type="monotone" dataKey="ENERGY" stroke="#8884d8" />
           <Line type="monotone" dataKey="WATER" stroke="#82ca9d" />
-          {/* <Area
-            type="monotone"
-            dataKey="Total"
-            stroke="#8884d8"
-            fillOpacity={1}
-            fill="url(#total)"
-          /> */}
         </LineChart>
       </ResponsiveContainer>
     </div>
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,46 +1,52 @@
 import "./featured.scss";
+import {useState} from 'react'
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
 
-const Featured = () => {
+const Featured = ({value}) => {
+  const [currentDate, setCurrentDate] = useState('');
+  const date = new Date();
+  const options = { day: "2-digit", month: "short", year: "numeric" };
+  // const formattedDate = date.toLocaleDateString("en-US", options)
+  // setCurrentDate(date.toLocaleDateString("en-US", options));
   return (
     <div className="featured">
       <div className="top">
-        <h1 className="title">Total Revenue</h1>
+        <h1 className="title"></h1>
         <MoreVertIcon fontSize="small" />
       </div>
       <div className="bottom">
         <div className="featuredChart">
           <CircularProgressbar value={70} text={"70%"} strokeWidth={5} />
         </div>
-        <p className="title">Total sales made today</p>
-        <p className="amount">$420</p>
+        <p className="title">{date.toLocaleDateString("vi-VN", options)}</p>
+        <p className="amount">{value} VND</p>
         <p className="desc">
-          Previous transactions processing. Last payments may not be included.
+          Bảng báo giá tiền điện nước
         </p>
         <div className="summary">
           <div className="item">
             <div className="itemTitle">Target</div>
             <div className="itemResult negative">
               <KeyboardArrowDownIcon fontSize="small"/>
-              <div className="resultAmount">$12.4k</div>
+              <div className="resultAmount">400.000VND</div>
             </div>
           </div>
-          <div className="item">
+          {/* <div className="item">
             <div className="itemTitle">Last Week</div>
             <div className="itemResult positive">
               <KeyboardArrowUpOutlinedIcon fontSize="small"/>
               <div className="resultAmount">$12.4k</div>
             </div>
-          </div>
+          </div> */}
           <div className="item">
             <div className="itemTitle">Last Month</div>
             <div className="itemResult positive">
               <KeyboardArrowUpOutlinedIcon fontSize="small"/>
-              <div className="resultAmount">$12.4k</div>
+              <div className="resultAmount">312.000VND</div>
             </div>
           </div>
         </div>
